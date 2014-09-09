ad7aaf7e3bd87c608825f2148a42080c109d834d
Le Labourier Marc <marc.lelabourier@gmail.com>
2014-09-09 16:41:51 -0700

    Add Video Background

---
+++ Lead-It/Assets/Script/backgroundvideo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+
+public class backgroundvideo : MonoBehaviour {
+
+	public MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.mainTexture = movie as MovieTexture;
+		audio.clip = movie.audioClip;
+		movie.loop = true;
+		movie.Play ();
+		audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}

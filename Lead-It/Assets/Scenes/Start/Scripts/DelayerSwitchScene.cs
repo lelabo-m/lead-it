4d58e6ad26573cee9d2b6ca6cff2d4e242f2a45f
millonc <millon.christopher@gmail.com>
2014-09-09 11:50:15 +0200

    Added starting_scene and scene switcher

---
+++ Lead-It/Assets/Scenes/Start/Scripts/DelayerSwitchScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayerSwitchScene : MonoBehaviour
+{
+    public float SecondsOfDelay;
+
+    void Update()
+    {
+        SecondsOfDelay -= Time.deltaTime;
+        if (SecondsOfDelay < 0)
+            Application.LoadLevel("game_menu");
+    }
+}

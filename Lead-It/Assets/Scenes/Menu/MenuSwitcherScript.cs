905c7a78402603ac35a4fd4d02ceb939c90c2d45
millonc <millon.christopher@gmail.com>
2014-09-09 14:22:08 +0200

    Menu: created settings

--- Lead-It/Assets/Scenes/Menu/MenuSwitcherScript.cs
+++ Lead-It/Assets/Scenes/Menu/MenuSwitcherScript.cs
@@ -3,18 +3,23 @@ using System.Collections;
 
 public class MenuSwitcherScript : MonoBehaviour
 {
-    public void GoGame()
-    {
+    public GameObject MainMenu;
+    public GameObject SetupGameMenu;
+    public GameObject SettingsMenu;
+    public GameObject CreditsMenu;
 
+    public void ShowSetupGame()
+    {
+        NGUITools.SetActive(MainMenu, false);
     }
 
-    public void GoSettings()
+    public void ShowSettings()
     {
-
+        NGUITools.SetActive(MainMenu, false);
     }
 
-    public void GoCredits()
+    public void ShowCredits()
     {
-
+        NGUITools.SetActive(MainMenu, false);
     }
 }
---
+++ Lead-It/Assets/Scenes/Menu/Settings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Settings : MonoBehaviour
+{
+    private bool _sound;
+    public bool Sound
+    {
+        get { return _sound ;}
+        set { _sound = value ;}
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+}

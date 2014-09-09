51f5de1b4ff8d457b75a3f2ab4dfb579fc3c3b6b
millonc <millon.christopher@gmail.com>
2014-09-09 12:31:21 +0200

    menu_scene: Added Settings and Credits buttons

---
+++ Lead-It/Assets/Scenes/Menu/MenuSwitcherScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSwitcherScript : MonoBehaviour
+{
+    public void GoGame()
+    {
+
+    }
+
+    public void GoSettings()
+    {
+
+    }
+
+    public void GoCredits()
+    {
+
+    }
+}

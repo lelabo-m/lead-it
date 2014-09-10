da27cde38eb8a6a5ad43e1020833da9eb14fab69
Le Labourier Marc <marc.lelabourier@gmail.com>
2014-09-10 14:12:35 +0200

    Add Country + ElemCountry script

---
+++ Lead-It/Assets/Script/Country.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Country : MonoBehaviour {
+
+	private string		CountryName;
+	private uint		Budget;
+	private float		DayRatio;
+	private uint		Profit;
+	private uint		Expense;
+
+	// Use this for initialization
+	void Start () {
+		this.CountryName = "";
+		this.Budget = 0;
+		this.DayRatio = 0.0f;
+		this.Profit = 0;
+		this.Expense = 0;
+	}
+
+	// Get all the aid / taxes / invest values to calc the result of the week
+	void UpdateCountry() {
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Getter / Setter
+	string getName() {
+		return this.CountryName;
+	}
+
+	void setName(string name) {
+			this.CountryName = name;
+	}
+
+	float getDayRatio() {
+		return this.DayRatio;
+	}
+
+	void setDayRatio(float val) {
+		this.DayRatio = val;
+	}
+
+	uint setBudget() {
+		return this.Budget;
+	}
+	
+	void getBudget(uint val) {
+		this.Budget = val;
+	}
+
+	uint setProfit() {
+		return this.Profit;
+	}	
+
+	void getProfit(uint val) {
+		this.Profit = val;
+	}
+
+	uint setExpense() {
+		return this.Expense;
+	}
+	
+	void getExpense(uint val) {
+		this.Expense = val;
+	}	
+}
---
+++ Lead-It/Assets/Script/CountryElem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountryElem : MonoBehaviour {
+
+	public enum ElemType
+	{
+		Aid,
+		Taxe,
+		Invest
+	}
+
+	public ElemType	Etype;
+	public string	Ename;
+	public uint		SliderVal;
+	public uint		ProfitBonus;
+	public uint		ExpenseMalus;
+	public uint		ProfitPercent;
+	public uint		ExpensePercent;
+
+	// Use this for initialization
+	void Start () {
+		this.SliderVal = 0;
+		this.ProfitBonus = 0;
+		this.ProfitPercent = 0;
+		this.ExpenseMalus = 0;
+		this.ExpensePercent = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	// Update sliderval
+	}
+}

fd17aa501144ab85d84d55b245b766a70e05decf
kunt22 <guillaumejparis@gmail.com>
2014-09-10 03:54:11 +0200

    maj complete menu

--- Lead-It/Assets/Scenes/Menu/MenuSwitcherScript.cs
+++ Lead-It/Assets/Scenes/Menu/MenuSwitcherScript.cs
@@ -5,6 +5,7 @@ public class MenuSwitcherScript : MonoBehaviour
 {
     public GameObject MainMenu;
     public GameObject SetupGameMenu;
+	public GameObject HowtoMenu;
     public GameObject SettingsMenu;
     public GameObject CreditsMenu;
 
@@ -13,6 +14,7 @@ public class MenuSwitcherScript : MonoBehaviour
         NGUITools.SetActive(SetupGameMenu, false);
         NGUITools.SetActive(SettingsMenu, false);
         NGUITools.SetActive(CreditsMenu, false);
+		NGUITools.SetActive(HowtoMenu, false);
     }
 
     public void ShowSetupGame()
@@ -20,9 +22,16 @@ public class MenuSwitcherScript : MonoBehaviour
         Application.LoadLevel(2);
     }
 
+	public void ShowHowto()
+	{
+		NGUITools.SetActive(MainMenu, false);
+		NGUITools.SetActive(HowtoMenu, true);
+	}
+
     public void ShowSettings()
     {
         NGUITools.SetActive(MainMenu, false);
+		NGUITools.SetActive(SettingsMenu, true);
     }
 
     public void ShowCredits()
@@ -30,4 +39,19 @@ public class MenuSwitcherScript : MonoBehaviour
         NGUITools.SetActive(MainMenu, false);
 		NGUITools.SetActive(CreditsMenu, true);
     }
+	public void ShowMenuQuitCredits()
+	{
+		NGUITools.SetActive(MainMenu, true);
+		NGUITools.SetActive(CreditsMenu, false);
+	}
+	public void ShowMenuQuitHowto()
+	{
+		NGUITools.SetActive(MainMenu, true);
+		NGUITools.SetActive(HowtoMenu, false);
+	}
+	public void ShowMenuQuitSettings()
+	{
+		NGUITools.SetActive(MainMenu, true);
+		NGUITools.SetActive(SettingsMenu, false);
+	}
 }

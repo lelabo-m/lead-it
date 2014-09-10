b9d208809444cea720ed5a0097de1adcc945d8ce
Le Labourier Marc <marc.lelabourier@gmail.com>
2014-09-10 23:45:46 +0200

    plop

---
+++ Lead-It/Assets/Script/CountryResources.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountryResources : MonoBehaviour {
+
+	public int	Budget;
+	public int	Profit;
+	public int	Expense;
+	public int	Popularity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

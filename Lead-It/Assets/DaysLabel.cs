1f32474f11540b70562231032a116e01a584f131
millonc <millon.christopher@gmail.com>
2014-09-11 14:18:21 +0200

    Scores gameover

---
+++ Lead-It/Assets/DaysLabel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DaysLabel : MonoBehaviour
+{
+    public LastScoreCompo scores;
+
+    void Start()
+    {
+        this.gameObject.GetComponent<UILabel>().text = "You successfully managed during\n" + scores.GetDay().ToString() + " Days";
+    }
+}
--- Lead-It/Assets/Script/LastScoreCompo.cs
+++ Lead-It/Assets/Script/LastScoreCompo.cs
@@ -1,57 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LastScoreCompo : MonoBehaviour {
+public class LastScoreCompo : MonoBehaviour
+{
+    private decimal DayScore;
+    private string Country;
+    private int WhichDie;
 
-	private decimal	DayScore;
-	private string	Country;
-	private int		WhichDie;
+    // Use this for initialization
+    void Awake()
+    {
+        DayScore = 0;
+        Country = "";
+        WhichDie = 0;
+        DontDestroyOnLoad(transform.gameObject);
+    }
 
-	// Use this for initialization
-	void Start () {
-		DayScore = 0;
-		Country = "";
-		WhichDie = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
-	void Awake() {
-		DontDestroyOnLoad (transform.gameObject);
-		}
+    }
 
+    // Getter/Setter
+    public void SetDay(decimal val)
+    {
+        DayScore = val;
+    }
 
-	// Getter/Setter
-	public void SetDay(decimal val)
-	{
-		DayScore = val;
-	}
+    public decimal GetDay()
+    {
+        return DayScore;
+    }
 
-	public decimal GetDay()
-	{
-		return DayScore;
-	}
+    public void SetCountry(string val)
+    {
+        Country = val;
+    }
 
-	public void SetCountry(string val)
-	{
-		Country = val;
-	}
+    public string GetCountry()
+    {
+        return Country;
+    }
 
-	public string GetCountry()
-	{
-		return Country;
-	}
+    public void SetDie(int val)
+    {
+        WhichDie = val;
+    }
 
-	public void SetDie(int val)
-	{
-		WhichDie = val;
-	}
-
-	public int GetDie()
-	{
-		return WhichDie;
-	}
+    public int GetDie()
+    {
+        return WhichDie;
+    }
 }
